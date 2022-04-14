1348a77f00385fd64249a0ca896560b1484d9eef
Humaira55 <59912446+Humaira55@users.noreply.github.com>
2022-04-14 15:56:55 +0530

    Add files via upload

---
+++ JavaLabWork/Assignment-3/DaysMenu.java
@@ -0,0 +1,55 @@
+package Assignment_3;
+
+import java.util.Scanner;
+
+public class DaysMenu {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int choice;
+		boolean quite = false;
+		
+		do {
+			System.out.println("Enter the choice between 1 to 7");
+			System.out.println("Press 0 to quite");
+			choice = sc.nextInt();
+			
+			switch(choice)
+			{
+			case 1 : System.out.println("Monday");
+					  break;
+					  
+			case 2 : System.out.println("Tuesday");
+			  		 break;
+			  
+			case 3 : System.out.println("Wednesday");
+			  		 break;
+			  
+			case 4 : System.out.println("Thursday");
+			  		 break;
+			  
+			case 5 : System.out.println("Friday");
+			  		 break;
+			  
+			case 6 : System.out.println("Saturday");
+			  		 break;
+			  
+			case 7 : System.out.println("Sunday");
+			  		 break;
+			  
+			case 0 : quite = true;
+			  		 break;
+			  
+			default : System.out.println("Wrong Choice");
+			  		  break;
+			}
+				System.out.println("");
+		}while(!quite);
+			System.out.println("Have a Nice day!");
+			
+		}
+
+	}
+
+
---
+++ JavaLabWork/Assignment-3/EmployeeSalary.java
@@ -0,0 +1,14 @@
+package Assignment_3;
+
+public class EmployeeSalary {
+	static int avg = 10000;
+	static int e1 = 11000;
+	static int e2 = 5000;
+
+	public static void main(String[] args) {
+		
+		int e3 = 3*avg -(e1 + e2) ;
+		System.out.println("The Third employee wii earn " + e3);
+		}
+
+}
