6a684a14ce63823bd827852f80c754edced6fb63
Humaira55 <59912446+Humaira55@users.noreply.github.com>
2022-04-21 16:34:00 +0530

    Add files via upload

---
+++ JavaLabWork/Assignment-5/Multilevel_Hierar.java
@@ -0,0 +1,55 @@
+package Assignment_4;
+
+class variable{
+	int a = 30;
+	int b= 20;
+}
+
+class addition extends variable{
+	int add=0;
+	void add()
+	{
+		add = a+b;
+		System.out.println("Addition of nember is : "+ add);
+	}	
+}
+
+class average extends addition{
+	void avg()
+	{
+		int avg = add/2;
+		System.out.println("Average of Number is :" + avg);
+	}	
+}
+
+class subtraction extends variable{
+	void sub()
+	{
+		int sub = a-b;
+		System.out.println("Subtraction of nember is : "+ sub);
+	}
+}
+
+class multiplication extends variable{
+	void mul()
+	{
+		int mul = a*b;
+		System.out.println("Multiplication of nember is : "+ mul);
+	}
+}
+
+public class Multilevel_Hierar {
+
+	public static void main(String[] args) {
+		average a1 = new average();
+		subtraction a2 = new subtraction();
+		multiplication a3 = new multiplication();
+		average a4 = new average();
+		a1.add();
+		a4.avg();
+		a2.sub();
+		a3.mul();
+
+	}
+
+}
