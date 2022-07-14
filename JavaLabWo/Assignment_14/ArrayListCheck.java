9ee50ed13446972eced97317cba709a186325fb3
Humaira55 <59912446+Humaira55@users.noreply.github.com>
2022-07-14 16:10:39 +0530

    Add files via upload

---
+++ JavaLabWo/Assignment_14/ArrayListCheck.java
@@ -0,0 +1,27 @@
+package Assignment_14;
+
+import java.util.ArrayList;
+
+public class ArrayListCheck {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> a1 = new ArrayList<String>();
+		
+		a1.add("Apple");
+		a1.add("Banana");
+		a1.add("Grapes");
+		a1.add("Mango");
+		a1.add("Orange");
+		
+		ArrayList<String> a2 = new ArrayList<String>();
+		
+		a2.add("Grapes");
+		a2.add("Mango");
+		a2.add("Orange");
+		
+		System.out.println("First ArrayList contains all the elements of Second ArrayList : " +a1.containsAll(a2));
+
+	}
+
+}
---
+++ JavaLabWo/Assignment_14/EmployeeMain.java
@@ -0,0 +1,62 @@
+package Assignment_14;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+		
+		Scanner sc1 = new Scanner(System.in);
+		Scanner sc2 = new Scanner(System.in);
+		int x;
+		
+		ArrayList<Employee> a1 = new ArrayList<Employee>();
+		
+		try {
+			
+			do {
+				
+				System.out.println("Enter Employee id : ");
+				int id = sc1.nextInt();
+				System.out.println("Enter Employee Name : ");
+				String name = sc2.nextLine();
+				System.out.println("Enter Employee Department : ");
+				String dept = sc2.nextLine();
+				
+				Employee e = new Employee(id,name,dept);
+				a1.add(e);
+				
+				System.out.println("For Adding more Employees press 1");
+				System.out.println("For quit press 0");
+				x = sc1.nextInt();
+				
+				if(x==0)
+				{
+					System.out.println("Thankyou!");
+				}
+				else if(x!=1)
+				{
+					throw new EmployeeInputException();
+				}
+			}while(x==1);
+		}
+		catch(EmployeeInputException e)
+		{
+			System.out.println();
+		}
+		
+		HashSet<Employee> h = new HashSet<Employee>(a1);
+		
+		Iterator itr = h.iterator();
+		
+		while(itr.hasNext())
+		{
+			System.out.println(itr.next());
+		}
+
+	}
+
+}
